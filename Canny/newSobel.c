fb83b24be1c812c1ce79455f639a3a8d6e209f3d
Carlos Garzon <showlove@knights.ucf.edu>
2016-09-09 00:08:00 -0400

    got convolution to work in function but still not scanning

--- Canny/newSobel.c
+++ Canny/newSobel.c
@@ -8,7 +8,21 @@
         int outpicy[256][256];
 
         int maskx[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
+        int maskxPadded[7][7] = {{0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0},
+                                 {0,0,-1,0,1,0,0},
+                                 {0,0,-2,0,2,0,0},
+                                 {0,0,-1,0,1,0,0},
+                                 {0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0},};
         int masky[3][3] = {{1,2,1},{0,0,0},{-1,-2,-1}};
+        int maskyPadded[7][7] = {{0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0},
+                                 {0,0,1,2,1,0,0},
+                                 {0,0,0,0,0,0,0},
+                                 {0,0,-1,-2,-1,0,0},
+                                 {0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0},};
         double ival[256][256],maxival;
 
 //mpcen/RobotVision TODO: remove comments
@@ -17,11 +31,15 @@
 //http://www-scf.usc.edu/~boqinggo/Canny.htm
 // marrh.2  from 82 down ignore
 
+void populateZeroArray(int paddedZeros[7][7], int pic[256][256], int i, int j);
+int getSumX(int paddedZeros[7][7], int pic[256][256], int i, int j);
+int getSumY(int paddedZeros[7][7], int pic[256][256], int i, int j); 
+
 main(argc,argv)
 int argc;
 char **argv;
 {
-  int i,j,p,q,mr,sum1,sum2;
+  int i,j,p,q,mr,sum1,sum2, pixel_i, pixel_j;
   double threshold, threshold2;
   FILE *fpOut, *fpLowThr, *fpHighThr, *fpImmage, *fopen();
   char *foobar;
@@ -60,35 +78,40 @@ char **argv;
   fpLowThr=fopen("lowthr.pgm", "wb");
   fpHighThr=fopen("highthr.pgm", "wb");
 
+  ///////////////////////
   // Get and store image
+  /////////////////////
   for (i=0;i<256;i++)
   {
     for (j=0;j<256;j++)
     {
       pic[i][j]  =  getc (fpImmage);
-      pic[i][j]  &= 0x00FF;  //Somehow in hex 00ff
+      pic[i][j]  &= 0x00FF;
     }
   }
+
+  int paddedZeros[7][7];
+  memset(paddedZeros, 0, sizeof(pic[0][0]) * 7 * 7);
+  //////////////////////////////////////////////////
+  //Scanning Convolution lec1-conv slide 11
+  /////////////////////////////////////////////////
+  mr = 1;
+  for ( pixel_i = mr; pixel_i <256-mr; pixel_i++)
+  { for (pixel_j=mr;pixel_j<256-mr;pixel_j++)
+    {
+        sum2 = 0; sum1 = 0;
+        //////////////////////////////////////////////////
         //Calculate the wheighted Sum lec-2 Sobel slide 1
-        mr = 1;
-        for (i=mr;i<256-mr;i++)
-        { for (j=mr;j<256-mr;j++)
-          {
-             sum1 = 0;
-             sum2 = 0;
-             for (p=-mr;p<=mr;p++)
-             {
-                for (q=-mr;q<=mr;q++)
-                {
-                   sum1 += pic[i+p][j+q] * maskx[p+mr][q+mr];
-                   sum2 += pic[i+p][j+q] * masky[p+mr][q+mr];
-                }
-             }
-             //Store wheighted sum
-             outpicx[i][j] = sum1;
-             outpicy[i][j] = sum2;
-          }
-        }
+        /////////////////////////////////////////////////
+
+        sum1 = getSumX( paddedZeros, pic, pixel_i, pixel_j);
+        sum2 = getSumY( paddedZeros, pic, pixel_i, pixel_j);      
+        //Store wheighted sum
+        outpicx[pixel_i][pixel_j] = sum1;
+        outpicy[pixel_i][pixel_j] = sum2;
+    }
+  }
+  mr = 1;
 
         maxival = 0;
         for (i=mr;i<256-mr;i++)
@@ -143,4 +166,63 @@ char **argv;
         }
 
 
+}
+
+void populateZeroArray(int paddedZeros[7][7], int pic[256][256], int i, int j)
+{
+
+  memset(paddedZeros, 0, sizeof(pic[0][0]) * 7 * 7);
+  
+  int mr = 1;
+  for(int x = -mr; x <= mr; x++){
+    for(int y = -mr; y <= mr; y++){
+      //2->4              //mr =  -1 -> 1
+      paddedZeros[x+3][y+3] = pic[i+x][j+y];
+    }
+  }
+}
+
+int getSumX(int paddedZeros[7][7], int pic[256][256], int i, int j)
+{
+
+  memset(paddedZeros, 0, sizeof(pic[0][0]) * 7 * 7);
+
+  int mr = 1;
+  int sum = 0;
+  for(int x = -mr; x <= mr; x++){
+    for(int y = -mr; y <= mr; y++){
+      //2->4              //mr =  -1 -> 1
+      paddedZeros[x+3][y+3] = pic[i+x][j+y];
+    }
+  }
+
+  for( int i = 0; i < 6; i ++ ){
+    for( int j = 0; j < 6; j++ ){
+      sum += paddedZeros[i][j] * maskxPadded[i][j];
+    }
+  }
+
+  return sum;
+}
+
+int getSumY(int paddedZeros[7][7], int pic[256][256], int i, int j)
+{
+  memset(paddedZeros, 0, sizeof(pic[0][0]) * 7 * 7);
+
+  int mr = 1;
+  int sum = 0;
+  for(int x = -mr; x <= mr; x++){
+    for(int y = -mr; y <= mr; y++){
+      //2->4              //mr =  -1 -> 1
+      paddedZeros[x+3][y+3] = pic[i+x][j+y];
+    }
+  }
+
+  for( int i = 0; i < 6; i ++ ){
+    for( int j = 0; j < 6; j++ ){
+      sum += paddedZeros[i][j] * maskyPadded[i][j];
+    }
+  }
+
+  return sum;
 }
